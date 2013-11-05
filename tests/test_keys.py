1e1c5c21c47ae246f03bf101ee656c624e2ea668
Rui Carmo <rui.carmo@gmail.com>
2013-11-05 23:50:35 +0000

    Fixtures

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -8,18 +8,28 @@ from miniredis.client import RedisClient
 
 pid = None
 
-def setup():
+def setup_module():
 	global pid
 	pid = miniredis.server.fork()
+	print pid
+	time.sleep(1)
 
-def teardown():
+def teardown_module():
 	global pid
 	os.kill(pid, signal.SIGKILL)
-
 
 
 r = RedisClient()
 
 def test_put():
-    eq_(r.put('test:key', 'value'),None)
+    eq_(r.set('test:key', 'value'),'OK')
 
+def test_get():
+	eq_(r.get('test:key'),'value')
+
+def test_del():
+	eq_(r.delete('test:key'),1)
+	eq_(r.get('test:key'),None)
+	r.set('test:key1', 'value')
+	r.set('test:key2', 'value')
+	eq_(r.delete('test:key1', 'test:key2'),2)
